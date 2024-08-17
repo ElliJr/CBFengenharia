2c4117316a5c629b2afc581b57a751ad330c4356
Elli junior <ellijuniorsantosdejesus8@gmail.com>
2024-08-17 15:04:01 -0300

    update  0.7

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 function toggleMenu() {
   const menu = document.getElementById('menu')
 
-  if (menu.style.left === '-200px') {
+  if (menu.style.left === '-400px') {
     menu.style.left = '0';
   } else {
-    menu.style.left = '-200px'
+    menu.style.left = '-400px'
   }
 }
 
